684f0df79ce86a4eb029ff9775fcccdc7b4a5d06
Simhateja17 <mg8751721@gmail.com>
2025-06-27 13:11:57 +0530

    Issue getting fixed v0

--- webpack.config.js
+++ webpack.config.js
@@ -2,45 +2,53 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = {
-  mode: 'development',
-  entry: './src/index.ts',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: 'index.html',
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        { from: 'src/assets', to: 'assets', noErrorOnMissing: true },
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+  
+  return {
+    mode: argv.mode || 'development',
+    entry: './src/index.ts',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+      publicPath: '/',
+    },
+    resolve: {
+      extensions: ['.ts', '.js'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
       ],
-    }),
-  ],
-  devServer: {
-    static: './dist',
-    port: process.env.PORT || 3000,
-    open: true,
-    hot: true,
-  },
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: 'index.html',
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          { from: 'src/assets', to: 'assets', noErrorOnMissing: true },
+        ],
+      }),
+    ],
+    devServer: {
+      static: './dist',
+      port: process.env.PORT || 3000,
+      host: '0.0.0.0',
+      allowedHosts: 'all',
+      open: false,
+      hot: true,
+      historyApiFallback: true,
+    },
+  };
 }; 
