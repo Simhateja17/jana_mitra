3688fd7d46adc21f98df784fe349c990931a0954
Simhateja17 <mg8751721@gmail.com>
2025-07-06 10:42:27 +0530

    Comit 6

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -85,6 +85,7 @@ export const projects: Project[] = [
             'Renigunta Railway Junction (15 Min. Drive)',
             'Tirupati city (25 Min. Drive)'
         ],
+        mapEmbed: '<iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3609.4424517888306!2d79.54318308551481!3d13.834295467869033!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTPCsDUwJzAzLjMiTiA3OcKwMzInNDQuMCJF!5e1!3m2!1sen!2sin!4v1751778521563!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
         additionalInfo: {
             landParcel: '40.55 Acres',
             floor: '16 Plots'
